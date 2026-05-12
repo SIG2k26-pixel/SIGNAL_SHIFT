5791fe251862a8145dfcd19081a060aecadec87e
SSOORYA007 <soorya04.01.2006@gmail.com>
2026-05-12 18:28:34 +0530

    Fix: trust proxy for Render

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('trust proxy', true); // Trust Render's proxy
 
 app.set('trust proxy', 1);
 
